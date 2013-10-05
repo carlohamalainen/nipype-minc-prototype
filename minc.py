84b192d57aadc10dfb2e704f19fc58532214f381
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-10-05 21:47:25 +0200

    Wrapper for minccopy.

--- minc.py
+++ minc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 # Synopsis: investigation of MINC support for Nipype.
 # Author: Carlo Hamalainen <carlo@carlo-hamalainen.net>
@@ -46,7 +46,7 @@ class ToRawInputSpec(StdOutCommandLineInputSpec):
     write_long = traits.Bool(
                 desc='Superseded by -int',
                 argstr='-long',)
- 
+
     write_float = traits.Bool(
                 desc='Write out data as single precision floating-point values',
                 argstr='-float',)
@@ -149,19 +149,84 @@ class ConvertTask(CommandLine):
     cmd = 'mincconvert'
 
     def _list_outputs(self):
+        # FIXME seems generic, is this necessary?
+        outputs = self.output_spec().get()
+        outputs['output_file'] = self.inputs.output_file
+        return outputs
+
+
+
+class CopyInputSpec(CommandLineInputSpec):
+    """
+    Implement minccopy? Its man page says:
+
+        NOTE: This program is intended primarily for use with scripts such
+        as mincedit.  It does not follow the typical design rules of most MINC
+        command-line tools and therefore should be  used only with caution.
+
+    It doesn't run on a standard MNC file, e.g.
+
+        $ minccopy /home/carlo/tmp/foo.mnc /tmp/foo_copy.mnc
+        (from miopen): Can't write compressed file
+        ncvarid: ncid -1: NetCDF: Not a valid ID
+
+    """
+
+    input_file = File(
+                    desc='input file to copy',
+                    exists=True,
+                    mandatory=True,
+                    argstr='%s',
+                    position=-2,)
+
+    output_file = File(
+                    desc='output file',
+                    mandatory=True,
+                    genfile=False,
+                    argstr='%s',
+                    position=-1,)
+
+    pixel_values = traits.Bool(
+                desc='Copy pixel values as is.',
+                argstr='-pixel_values',)
+
+    real_values = traits.Bool(
+                desc='Copy real pixel intensities (default).',
+                argstr='-real_values',
+                usedefault=True,)
+
+class CopyOutputSpec(TraitedSpec):
+    # FIXME Am I defining the output spec correctly?
+    output_file = File(
+                    desc='output file',
+                    exists=True,)
+
+class CopyTask(CommandLine):
+    input_spec  = CopyInputSpec
+    output_spec = CopyOutputSpec
+    cmd = 'minccopy'
+
+    def _list_outputs(self):
+        # FIXME seems generic, is this necessary?
         outputs = self.output_spec().get()
         outputs['output_file'] = self.inputs.output_file
         return outputs
 
 if __name__ == '__main__':
-    c = ConvertTask(input_file='/home/carlo/tmp/foo.mnc', output_file='/tmp/foo.mnc', two=True, clobber=True, compression=3, chunk=2, template=True)
-    print c.cmdline
-    c.run()
-    
+    convert = ConvertTask(input_file='/home/carlo/tmp/foo.mnc', output_file='/tmp/foo.mnc', two=True, clobber=True, compression=3, chunk=2, template=True)
+    print convert.cmdline
+    convert.run()
+
     print
     print
 
-    r = ToRawTask(input_file='/home/carlo/tmp/foo.mnc', normalize=True)
-    print r.cmdline
-    r.run()
+    toraw = ToRawTask(input_file='/home/carlo/tmp/foo.mnc', normalize=True)
+    print toraw.cmdline
+    toraw.run()
+
+    print
+    print
+
+    copy = CopyTask(input_file='/home/carlo/tmp/foo.mnc', output_file='/tmp/foo_copy.mnc')
+    print copy.cmdline
 
